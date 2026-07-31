2fced29cd211aa1c61e3f60d9a6f4e6fc48a8b6a
AANCode <122744819+AANCode@users.noreply.github.com>
2026-07-31 10:48:05 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+List<Ticket> tickets = new List<Ticket>();
+
+Ticket ticket1 = new Ticket
+{
+    Id = 1,
+    Title = "mus",
+    Description = "Jeg trenger en ny mus",
+    Status = 0 // Open
+};
+
+tickets.Add(ticket1);
+
+foreach (var ticket in tickets)
+{
+    Console.WriteLine($"Ticket ID: {ticket.Id}, Title: {ticket.Title}");
+}
+
+
+foreach (var ticket in tickets)
+{
+    if (ticket.Id == 1)
+    {
+        ticket.Status = 1; // Update status to In Progress
+        Console.WriteLine($"Ticket ID: {ticket.Id} status updated to In Progress.");
+    }
+}
+
+for (int i = 0; i < tickets.Count; i++)
+{
+    if (tickets[i].Id == 1)
+    {
+        tickets.RemoveAt(i);
+        Console.WriteLine("Ticket has been removed.");
+    }
+}
---
+++ Ticket.cs
@@ -0,0 +1,8 @@
+public class Ticket 
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Status { get; set; } // 0 = Open, 1 = In Progress, 2 = Closed
+
+}
