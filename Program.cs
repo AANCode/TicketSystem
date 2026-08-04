40f2c0ee874fe9de31345249d9c3427a8a3ef8e9
AANCode <122744819+AANCode@users.noreply.github.com>
2026-08-04 13:49:10 +0200

    opprettet database

--- Program.cs
+++ Program.cs
@@ -1,36 +1,100 @@
-﻿
-List<Ticket> tickets = new List<Ticket>();
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.VisualBasic;
 
-Ticket ticket1 = new Ticket
+
+using var connection = new SqliteConnection("Data Source=support.db");
+connection.Open();
+
+
+string sql = @"
+    CREATE TABLE IF NOT EXISTS Tickets (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Title TEXT NOT NULL,
+        Description TEXT,
+        Status INTEGER NOT NULL
+    );
+";
+
+using var command = new SqliteCommand(sql, connection);
+command.ExecuteNonQuery();
+
+Console.WriteLine("Database og tabell er opprettet");
+
+
+string titleVariable = " ";
+string descriptionVariable = " ";
+string statusVariable = " ";
+
+string idVariable = " ";
+string nyStatus = " ";
+
+
+string valg = " ";
+
+while(valg != "0")
 {
-    Id = 1,
-    Title = "mus",
-    Description = "Jeg trenger en ny mus",
-    Status = 0 // Open
-};
+    Console.WriteLine("Hvilken oppgave skal du utføre:\n 1 for å opprette en ny sak \n 2 for å oppdatere en sak \n 3 for å se alle saker \n 0 for å avslutte programmet");
+    valg = Console.ReadLine();
 
-tickets.Add(ticket1);
-
-foreach (var ticket in tickets)
-{
-    Console.WriteLine($"Ticket ID: {ticket.Id}, Title: {ticket.Title}");
-}
-
-
-foreach (var ticket in tickets)
-{
-    if (ticket.Id == 1)
+    if (valg == "1")
     {
-        ticket.Status = 1; // Update status to In Progress
-        Console.WriteLine($"Ticket ID: {ticket.Id} status updated to In Progress.");
+        Console.WriteLine("Skriv inn Titlen til saken: ");
+        titleVariable = Console.ReadLine();
+
+        Console.WriteLine("Skriv inn Beskrivelse til saken: ");
+        descriptionVariable = Console.ReadLine();
+
+        Console.WriteLine("Skriv inn Status til saken(Skriv 0 for nye saker): ");
+        statusVariable = Console.ReadLine();
+
+
+        string insertsql = @"
+        INSERT INTO Tickets (Title, Description, Status)
+        VALUES (@title, @description, @status);
+        ";
+
+        using var insertcommand = new SqliteCommand(insertsql, connection);
+        insertcommand.Parameters.AddWithValue("@title", titleVariable);
+        insertcommand.Parameters.AddWithValue("@description", descriptionVariable);
+        insertcommand.Parameters.AddWithValue("@status", statusVariable);
+        insertcommand.ExecuteNonQuery();
+
+        Console.WriteLine("Saken har blitt lagt inn i Databasen");
     }
-}
-
-for (int i = 0; i < tickets.Count; i++)
-{
-    if (tickets[i].Id == 1)
+    else if(valg == "2")
     {
-        tickets.RemoveAt(i);
-        Console.WriteLine("Ticket has been removed.");
+        Console.WriteLine("Hva er ID-en til saken du vil redigere");
+        idVariable = Console.ReadLine();
+        
+        Console.WriteLine("hva er den nye statusen?: ");
+        nyStatus = Console.ReadLine();
+
+
+        string updatesql = @"
+        UPDATE Tickets SET Status = @status WHERE Id = @id;
+        ";
+
+        using var updateCommand = new SqliteCommand(updatesql, connection);
+        updateCommand.Parameters.AddWithValue("@status", nyStatus);
+        updateCommand.Parameters.AddWithValue("@id", idVariable);
+        updateCommand.ExecuteNonQuery();
+    }
+    else if(valg == "3")
+    {
+        string readersql =@"
+            SELECT* FROM Tickets;
+        ";
+
+        using var selectCommand = new SqliteCommand(readersql, connection);
+        using var reader = selectCommand.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["Id"]}, Title: {reader["Title"]}, Description: {reader["Description"]}, Status: {reader["Status"]}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ugylidig valg");
     }
 }
