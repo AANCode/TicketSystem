ee933d963824bf92e27833fcc1017dead6d32311
AANCode <122744819+AANCode@users.noreply.github.com>
2026-08-13 10:34:51 +0200

    assembly forandringer

--- obj/Debug/net10.0/TicketSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/TicketSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debaa4b2c4df1e459d41920bc1289ce5a7b51c56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7041d80c51eff1c5a9435c4418d71cff53e33d03")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
